67e639a15f3b36bb9bed51277a5e320442e7df48
unknown <barry.tickle12@gmail.com>
2024-03-26 18:52:31 +0000

    Updated the number field to allow for bigger numbers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                         Number to roman
                         </label>
-                        <input type="text" maxlength="5" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Number">
+                        <input type="text" maxlength="10" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Number">
                     </div>
                     <div class="flex items-center justify-between">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
